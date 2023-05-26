6ef9b51188f89ce734ee4e1bdc677f808411a9bf
Lars Stalder <88284345+LordofCoding06@users.noreply.github.com>
2023-05-26 08:59:32 +0200

    Update auslesen.ps1
    senin

--- auslesen.ps1
+++ auslesen.ps1
@@ -1,3 +1,35 @@
+function Suche-Navigiere-Verzeichnis {
+  param (
+      [Parameter(Mandatory = $true)]
+      [string]$Verzeichnis
+  )
+
+  # Durchsucht die Unterverzeichnisse rekursiv
+  $verzeichnisse = Get-ChildItem -Path $Verzeichnis -Directory
+
+  # Indizierte Ausgabe der Unterverzeichnisse
+  for ($i = 0; $i -lt $verzeichnisse.Count; $i++) {
+      Write-Host "[$i] $($verzeichnisse[$i].FullName)"
+  }
+
+  # Auswahl des Unterverzeichnisses
+  $auswahl = Read-Host "Geben Sie die Indexnummer des gewünschten Unterverzeichnisses ein (oder 'q' zum Beenden):"
+
+  if ($auswahl -eq "q") {
+      return
+  }
+
+  # Validierung der Auswahl
+  if ($auswahl -lt 0 -or $auswahl -ge $verzeichnisse.Count) {
+      Write-Host "Ungültige Auswahl. Bitte geben Sie eine gültige Indexnummer ein."
+      Suche-Navigiere-Verzeichnis -Verzeichnis $Verzeichnis
+      return
+  }
+
+  # Navigieren in das ausgewählte Unterverzeichnis
+  Suche-Navigiere-Verzeichnis -Verzeichnis $verzeichnisse[$auswahl].FullName
+}
+
 function Suche-XLS-Dateien {
   param (
     [Parameter(Mandatory = $true)]
@@ -16,6 +48,6 @@ function Suche-XLS-Dateien {
   }
 }
 
-# Beispielaufruf
 Clear-Host
-Suche-XLS-Dateien -Verzeichnis "Q:\AppTesting\QFTestFrameWork\QFTestDriver\Syrius\CEN_UNI"
+# Suche-XLS-Dateien -Verzeichnis "Q:\AppTesting\QFTestFrameWork\QFTestDriver\Syrius\CEN_UNI"
+Suche-Navigiere-Verzeichnis -Verzeichnis "Q:\AppTesting\QFTestFrameWork\QFTestDriver\Syrius\CEN_UNI"
