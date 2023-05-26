67bc4fcb7a9b28514a05da0f2948c04fa84c8074
Lars Stalder <88284345+LordofCoding06@users.noreply.github.com>
2023-05-26 08:40:47 +0200

    Update auslesen.ps1
    Moinsen

--- auslesen.ps1
+++ auslesen.ps1
@@ -1,2 +1,24 @@
-[String] $folder = "Q:\AppTesting\QFTestFrameWork\QFTestDriver\Syrius";
-Get-ChildItem -Path $folder | SELECT Attributes, Name, CreationTime | Format-Table -AutoSize;
+function Suche-XLS-Dateien {
+  param (
+    [Parameter(Mandatory = $true)]
+    [string]$Verzeichnis
+  )
+
+  # Durchsucht das Verzeichnis nach .xls-Dateien
+  $dateien = Get-ChildItem -Path $Verzeichnis -Filter "*.xls" -File
+
+  if ($dateien.Count -gt 0) {
+    Write-Host "Gefundene .xls-Dateien im Verzeichnis '$Verzeichnis':"
+    $dateien | foreach { Write-Host $_.FullName }
+  }
+
+  # Durchsucht die Unterverzeichnisse
+  $verzeichnisse = Get-ChildItem -Path $Verzeichnis -Directory
+
+  foreach ($verzeichnis in $verzeichnisse) {
+    Suche-XLS-Dateien -Verzeichnis $verzeichnis.FullName
+  }
+}
+
+# Beispielaufruf
+Suche-XLS-Dateien -Verzeichnis "C:\Pfad\Zum\Verzeichnis"
