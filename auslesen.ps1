73305bb30ec7cd0f59e6c5e0ce6d7596d787dac8
Lars Stalder <88284345+LordofCoding06@users.noreply.github.com>
2023-05-26 09:05:20 +0200

    Update auslesen.ps1
    noch nicht funktionable

--- auslesen.ps1
+++ auslesen.ps1
@@ -1,33 +1,50 @@
+$Verzeichnisverlauf = @()
+
 function Suche-Navigiere-Verzeichnis {
-  param (
-      [Parameter(Mandatory = $true)]
-      [string]$Verzeichnis
-  )
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Verzeichnis
+    )
 
-  # Durchsucht die Unterverzeichnisse rekursiv
-  $verzeichnisse = Get-ChildItem -Path $Verzeichnis -Directory
+    # Durchsucht die Unterverzeichnisse rekursiv
+    $verzeichnisse = Get-ChildItem -Path $Verzeichnis -Directory
 
-  # Indizierte Ausgabe der Unterverzeichnisse
-  for ($i = 0; $i -lt $verzeichnisse.Count; $i++) {
-      Write-Host "[$i] $($verzeichnisse[$i].FullName)"
-  }
+    # Indizierte Ausgabe der Unterverzeichnisse
+    for ($i = 0; $i -lt $verzeichnisse.Count; $i++) {
+        Write-Host "[$i] $($verzeichnisse[$i].FullName)"
+    }
 
-  # Auswahl des Unterverzeichnisses
-  $auswahl = Read-Host "Geben Sie die Indexnummer des gewünschten Unterverzeichnisses ein (oder 'q' zum Beenden):"
+    # Auswahl des Unterverzeichnisses
+    $auswahl = Read-Host "Geben Sie die Indexnummer des gewünschten Unterverzeichnisses ein (oder 'z' zum Zurückkehren, 'q' zum Beenden):"
 
-  if ($auswahl -eq "q") {
-      return
-  }
+    if ($auswahl -eq "q") {
+        return
+    }
 
-  # Validierung der Auswahl
-  if ($auswahl -lt 0 -or $auswahl -ge $verzeichnisse.Count) {
-      Write-Host "Ungültige Auswahl. Bitte geben Sie eine gültige Indexnummer ein."
-      Suche-Navigiere-Verzeichnis -Verzeichnis $Verzeichnis
-      return
-  }
+    if ($auswahl -eq "z") {
+        # Zurück zum vorherigen Verzeichnis
+        if ($Verzeichnisverlauf.Count -gt 0) {
+            $vorherigesVerzeichnis = $Verzeichnisverlauf[$Verzeichnisverlauf.Count - 1]
+            $Verzeichnisverlauf = $Verzeichnisverlauf[0..($Verzeichnisverlauf.Count - 2)]
+            Suche-Navigiere-Verzeichnis -Verzeichnis $vorherigesVerzeichnis
+        } else {
+            Write-Host "Sie befinden sich bereits im obersten Verzeichnis."
+            Suche-Navigiere-Verzeichnis -Verzeichnis $Verzeichnis
+        }
+        return
+    }
 
-  # Navigieren in das ausgewählte Unterverzeichnis
-  Suche-Navigiere-Verzeichnis -Verzeichnis $verzeichnisse[$auswahl].FullName
+    # Validierung der Auswahl
+    if ($auswahl -lt 0 -or $auswahl -ge $verzeichnisse.Count) {
+        Write-Host "Ungültige Auswahl. Bitte geben Sie eine gültige Indexnummer ein."
+        Suche-Navigiere-Verzeichnis -Verzeichnis $Verzeichnis
+        return
+    }
+
+    # Navigieren in das ausgewählte Unterverzeichnis
+    $neuesVerzeichnis = $verzeichnisse[$auswahl].FullName
+    $Verzeichnisverlauf += $Verzeichnis
+    Suche-Navigiere-Verzeichnis -Verzeichnis $neuesVerzeichnis
 }
 
 function Suche-XLS-Dateien {
